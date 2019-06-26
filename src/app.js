0d302f4ef86fd694c0e4127d526d840a96c324b2
ppatel81 <ppatel81@dxc.com>
2019-06-25 21:05:36 -0500

    removed code

--- src/app.js
+++ src/app.js
@@ -7,40 +7,4 @@ MongoClient.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true }, (err
     }
     console.log('Connected Successfully');
     const db = client.db('Task-Manager');
-    // db.collection('tasks').updateOne({
-    //     _id: new ObjectId("5d12ad0f3feb51061215e025")
-    // }, {
-    //     $set: {
-    //         description: 'Book a Rental car',
-    //         completed: true
-    //     }
-    // }).then((result) => {
-    //     console.log(result)
-    // }, (err) => {
-    //     console.log(err)
-    // })
-
-    // db.collection('users').updateOne({
-    //     name: 'Pragnesh'
-    // }, {
-    //     $inc: {
-    //         age: 1
-    //     }
-    // }).then((result) => {
-    //     console.log(result)
-    // }, (err) => {
-    //     console.log(err)
-    // })
-
-    db.collection('tasks').updateMany({
-        completed: true
-    }, {
-        $set: {
-            completed: false
-        }
-    }).then((result) => {
-        console.log(result)
-    }, (err) => {
-        console.log(err)
-    })
 });
