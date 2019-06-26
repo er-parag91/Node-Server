6985103305d08c9863fc66d4a5f1e8000bfcc742
ppatel81 <ppatel81@dxc.com>
2019-06-25 22:26:15 -0500

    Schema for User

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,26 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://177.0.0.1:27017/task-manager-api', {
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const Me = new User({
+    name: 'Parag',
+    age: 26
+})
+
+Me.save().then(me => {
+    console.log(me)
+}, (error) => {
+    console.log(error)
 })
