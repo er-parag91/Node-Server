7507fb5e5091463adf6e9e78f03d082b8e75d307
ppatel81 <ppatel81@dxc.com>
2019-06-25 20:04:29 -0500

    Read Operation

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-const mongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectId } = require('mongodb');
 
 
-mongoClient.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true }, (err, client) => {
+MongoClient.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true }, (err, client) => {
     if (err) {
         return console.log('error occured while connecting');
     }
     console.log('Connected Successfully');
     const db = client.db('Task-Manager');
 
+    // Insert one document
     db.collection('users').insertOne({
         name: 'Parag',
         age: 25
@@ -19,6 +20,8 @@ mongoClient.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true }, (err
         console.log(result.ops)
     })
 
+
+    // insert many documents
     db.collection('users').insertMany([
         {
             name: 'Pragnesh',
@@ -40,6 +43,8 @@ mongoClient.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true }, (err
         console.log(result.ops)
     });
 
+
+    // insert many documents
     db.collection('tasks').insertMany([
         {
             description: 'Buy grocery',
@@ -60,5 +65,27 @@ mongoClient.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true }, (err
 
         console.log(result.ops)
     });
+
+    // find one document based on query
+    db.collection('users').findOne({
+        name: 'Parag'
+    }, (error, result) => {
+        if (error) {
+            console.log('Unable to fetch')
+        }
+
+        console.log(result)
+    })
+    
+    // find one document based on query
+    db.collection('tasks').findOne({
+        _id: new ObjectId("5d12ad0f3feb51061215e025")
+    }, (error, result) => {
+        if (error) {
+            console.log('Unable to fetch')
+        }
+
+        console.log(result)
+    })
 
 });
