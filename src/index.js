f329bc605df9547dca3830cf7778f10d63a3f3f7
ppatel81 <ppatel81@dxc.com>
2019-06-26 00:08:16 -0500

    Read Users and Task Route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 require('./db/mongoose');
 
+// Schema
 const User = require('./models/user');
 const Task = require('./models/task');
 
+// express config
 const app = express();
-
 const port = process.env.PORT || 3000;
-
 app.use(express.json());
 
+
+// -------------------------- User routes -----------------------//
+
+// Create user - post request
 app.post('/users', (req, res) => {
     const user = new User(req.body);
 
@@ -22,6 +26,21 @@ app.post('/users', (req, res) => {
         })
 })
 
+// Read users - Get request
+app.get('/users', (req, res) => {
+    User.find({})
+        .then(users => {
+            res.status(200).send(users);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+});
+
+
+// -------------------------- Task routes -----------------------//
+
+// Create Task - post request
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
 
@@ -35,6 +54,19 @@ app.post('/tasks', (req, res) => {
 
 })
 
+// Read task - Get request
+app.get('/tasks', (req, res) => {
+    Task.find({})
+        .then(tasks => {
+            res.status(200).send(tasks);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+});
+
+
+// Listener
 app.listen(port, () => {
     console.log(`Server is up and running on ${port}`);
 });
