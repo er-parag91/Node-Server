3a54f1f4c111e84841a7ff7ad0609dc29907cd70
ppatel81 <ppatel81@dxc.com>
2019-06-26 17:23:37 -0500

    update task request validation and async/await embedded

--- src/index.js
+++ src/index.js
@@ -137,20 +137,25 @@ app.get('/tasks/:id', async (req, res) => {
 });
 
 // Find task by id and update - patch request
-app.patch('/tasks/:id', (req, res) => {
-    Task.findByIdAndUpdate({ _id: req.params.id}, {
-        description: 'Do laundry and press'
-    })
-    .then((task) => {
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        res.status(400).send({ error: 'Not valid updates' });
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
         if (!task) {
-            res.status(400).send('Error ocuured while updating');
+            res.status(404).send();
         }
         res.status(200).send(task);
-    })
-    .catch((error) => {
-        res.status(500).send(error);
-    })
-}) 
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
 
 // Find task by id and delete - delete request
 app.delete('/tasks/:id', (req, res) => {
