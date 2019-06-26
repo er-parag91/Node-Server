00215b3590235a6ff7945e2d4abd416eb1fd6bc1
ppatel81 <ppatel81@dxc.com>
2019-06-25 23:42:30 -0500

    destructured model and task model(schema) files

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,57 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is not valid');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value){
-            if (value < 0) {
-                throw new Error('Age must be positive integer');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 6,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Your password should not containe common words');
-            }
-        }
-    }
-});
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+export const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is not valid');
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value){
+            if (value < 0) {
+                throw new Error('Age must be positive integer');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Your password should not containe common words');
+            }
+        }
+    }
+});
