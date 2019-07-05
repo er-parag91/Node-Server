63645c87ba4b6642fd6530672ff3e40a991b9574
ppatel81 <ppatel81@dxc.com>
2019-07-05 15:08:52 -0500

    refactored task schema and timestamps get saved for new or update operation

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const Task = mongoose.model('Task', {
+
+const tasks = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -14,6 +15,11 @@ const Task = mongoose.model('Task', {
         type: mongoose.Schema.Types.ObjectId,
         required: true
     }
-});
+}, {
+    timestamps: true
+})
+
+
+const Task = mongoose.model('Task', tasks);
 
 module.exports = Task;
