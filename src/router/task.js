bf912fa4667cecfbe7d31331c7711a9dfa416694
ppatel81 <ppatel81@dxc.com>
2019-07-05 00:38:22 -0500

    authenticated delete task route

--- src/router/task.js
+++ src/router/task.js
@@ -85,19 +85,19 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 });
 
 // Find task by id and delete - delete request
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
 
     try {
-        const task = await Task.findByIdAndDelete(req.params.id);
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
         if (!task) {
             res.status(404).send({
                 error: 'Invalid delete operation'
             });
         }
-        res.status(200).send(task);
+        res.send(task)
 
     } catch (e) {
-        res.status(400).send(error);
+        res.status(400).send(e);
     }
 })
 
