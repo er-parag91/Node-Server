9bcdbe7f1d9628e76e5bb8c7727709c80c8f5a33
ppatel81 <ppatel81@dxc.com>
2019-07-08 18:43:28 -0500

    wiping out database before each test so there will be no deplicate or failing case & creating new user before each test suites

--- src/tests/users.test.js
+++ src/tests/users.test.js
@@ -3,8 +3,15 @@ const app = require('../app');
 
 const User = require('../models/user');
 
+const userOne = {
+    name: 'Test',
+    email: 'test@test.com',
+    password: 'Test123!!'
+}
+
 beforeEach(async () => {
     await User.deleteMany();
+    await new User(userOne).save();
 })
 
 test('should sign up the new user', async () => {
