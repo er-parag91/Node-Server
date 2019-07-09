5c37c87c6c8f7231c116b0d8ceb16d33f6ed0b86
ppatel81 <ppatel81@dxc.com>
2019-07-08 19:03:51 -0500

    get user profile with setting up header(token) and refactoring code

--- src/tests/users.test.js
+++ src/tests/users.test.js
@@ -1,12 +1,20 @@
 const request = require('supertest');
 const app = require('../app');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
 
 const User = require('../models/user');
 
+const userOneID = new mongoose.Types.ObjectId();
 const userOne = {
+    _id: userOneID,
     name: 'Test',
     email: 'test@test.com',
-    password: 'Test123!!'
+    password: 'Test123!!',
+    tokens: [{
+        token: jwt.sign({_id:userOneID}, process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async () => {
@@ -16,25 +24,40 @@ beforeEach(async () => {
 
 // test for signing up the user
 test('should sign up the new user', async () => {
-    await request(app).post('/users').send({
-        name: 'Parag Patel',
-        email: 'ppatel81@dev.com',
-        password: 'Red123!!'
-    }).expect(201);
+    await request(app)
+            .post('/users')
+            .send({
+                name: 'Parag Patel',
+                email: 'ppatel81@dev.com',
+                password: 'Red123!!'
+            }).expect(201);
 });
 
 // test for login the existing user
 test('should log in the user', async () => {
-    await request(app).post('/users/login').send({
-        email: 'test@test.com',
-        password: 'Test123!!'
-    }).expect(200);
+    await request(app)
+            .post('/users/login')
+            .send({
+                email: 'test@test.com',
+                password: 'Test123!!'
+            }).expect(200);
 })
 
 // test for not login the user with bad credentials
 test('should not log in the user for bad credential', async () => {
-    await request(app).post('/users/login').send({
-        email: userOne.email,
-        password: 'thisIsIncorrectPasswor!!'
-    }).expect(400);
+    await request(app)
+            .post('/users/login').
+            send({
+                email: userOne.email,
+                password: 'thisIsIncorrectPasswor!!'
+            }).expect(400);
+})
+
+// read or get user profile with setting up headers(token)
+test('should get the user profile', async() => {
+    await request(app)
+            .get('/users/me')
+            .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+            .send()
+            .expect(200);
 })
