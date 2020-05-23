5d1b09d97fb6f61cf395ed1735be18acd0d108a9
ppatel81 <ppatel81@dxc.com>
2020-05-23 14:53:23 -0500

    middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,18 +1,26 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-const auth = async (req, res, next) => {
+const auth = (req, res, next) => {
     try {
         token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
-        if (!user) {
-            throw new Error();
-        }
-        req.token = token;
-        req.user = user;
-        next();
-    } catch(e) {
+        return User.findOne({
+                _id: decoded._id,
+                'tokens.token': token
+            })
+            .then(user => {
+                if (!user) {
+                    throw new Error();
+                }
+                req.token = token;
+                req.user = user;
+                next();
+            })
+            .catch(e => {
+                res.status(401).send('Unauthenticated access to requested resource is prohibited!');
+            });
+    } catch (e) {
         res.status(401).send('Unauthenticated access to requested resource is prohibited!');
     }
 }
